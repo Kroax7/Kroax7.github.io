ef81974f7e4dd3435d55bb3c976f094e748fba3e
Aksel Jensen <github@kroax.net>
2024-05-18 18:39:15 +0200

    Initial commit

---
+++ assets/GameProjects-8f2a39e2.js
@@ -0,0 +1,181 @@
+import{P as e,a as t}from"./ProjectData-b99058f1.js";import{d as l,_ as r,r as o,c as n,e as c,a,o as p}from"./index-d6561eb9.js";const m=[new e("project-aitd","Alone in the Dark","img/projects/aitd/pk.png",`
+    <div class="paragraph">
+        <strong>Alone in the Dark</strong> is a third person survival horror game released in 2024 and developed by Pieces interactive, published by THQ Nordic. 
+        <br/>Developed in <strong>Unreal Engine</strong> and available on on <a href="https://store.steampowered.com/app/1310410/Alone_in_the_Dark/" target="_blank">PC</a>,       
+        <a href="https://store.playstation.com/sv-se/concept/10005283/" target="_blank">Playstation 5</a> and 
+        <a href="https://www.xbox.com/games/store/alone-in-the-dark/9mw67wg48xvx" target="_blank">XBOX Series X|S</a>.
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/6lyDlYbbqkY" frameborder="0" allowfullscreen></iframe>
+    </div>
+    
+    <div class="paragraph">
+        My role was a <strong>technical designer</strong> who worked on the project from it's earliest days with proof of 
+        concept up until a standalone demo and finally release. 
+        <br/>By the end of the project I also transitioned into acting <strong>Design Team Lead</strong> where I oversaw 
+        delegation of tasks and project planning as well as spearheaded a performance task force ahead of launch.
+    </div>
+    
+        <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/aitd/3.png" alt="Alone in the Dark Screenshot" />
+        <img class="pc-screenshot" src="img/projects/aitd/5.png" alt="Alone in the Dark Screenshot" />
+    </div>
+
+    <div class="paragraph">
+        My responsibilities on the project included:
+        <ul>
+            <li value="gameplay-ability-system">Gameplay Ability System</li>
+            <ul>
+                <li value="player-abilities">Player abilities</li>
+                <ul>
+                    <li value="ranged-weapons">Ranged weapons</li>
+                    <li value="melee-attacks-&-chains">Melee attacks & chains</li>
+                    <li value="dodging-&-movement">Dodging & movement</li>
+                    <li value="interaction-system">Interaction system</li>
+                </ul>
+                <li value="enemy-abilities">Enemy abilities</li>          
+                <li value="hit-reactions">Hit Reactions</li>
+                <li value="attribute-sets">Attribute sets</li>      
+            </ul>            
+            <li value="enemies">Enemies</li>
+            <ul>
+                <li value="the-dark-man">The Dark Man</li>
+                <ul>
+                    <li value="gameplay-design">Gameplay design</li>
+                    <li value="gameplay-implementation">Gameplay implementation</li>
+                    <li value="ai">AI</li>
+                    <li value="abilities">Abilities</li>
+                    <li value="obsession-system">Obsession system</li>
+                </ul>
+                <li value="burrower-monster">"Burrower" monster</li>
+                <ul>
+                    <li value="gameplay-design">Gameplay design</li>
+                    <li value="ai">AI</li>
+                    <li value="abilities-&-attacks">Abilities & attacks</li>
+                </ul>
+                <li value="eqs">EQS</li>
+            </ul>
+            <li value="balancing-&-difficulty">Balancing & difficulty</li>
+            <ul>
+                <li value="enemy-stats">Enemy stats</li>
+                <li value="difficulty-scaling">Difficulty scaling</li>
+                <li value="dynamic-pickup-system-&-balancing">Dynamic pickup system & balancing</li>                            
+            </ul>
+            <li value="doors">Doors</li>
+            <li value="puzzles">Puzzles</li>
+            <li value="ui">UI</li>
+            <li value="gameplay-camera">Gameplay camera</li>
+            <ul>
+                <li value="dynamic-camera-modifiers">Dynamic Camera Modifiers</li>
+                <li value="camera-shakes">Camera Shakes</li>
+                <li value="camera-settings">Camera settings</li>
+                <li value="retro-static-cameras">"Retro" static cameras</li>
+            </ul>
+            <li value="optimisation">Optimisation</li>
+            <ul>
+                <li value="unreal-insights">Unreal Insights</li>
+                <li value="performance-task-force-owner-&-organizer">Performance task force owner & organizer</li>
+            </ul>
+            <li value="flowgraph-setups"><a href="https://github.com/MothCocoon/FlowGraph">FlowGraph</a></li>
+            <ul>
+                <li value="anomalies">Anomalies</li>
+                <li value="level-events">Level events</li>
+                <li value="level-transition-loading-screens">Level transition loading screens</li>
+                <li value="flow-nodes">Flow Nodes</li>
+                <li value="mentoring">Mentoring</li>
+            </ul>            
+            <li value="tools">Tools</li>
+            <ul>
+                <li value="albedo-color-picker">Albedo color picker</li>
+                <li value="blueprint-library-functions">Blueprint Libraries</li>
+                <li value="editor-utility-blueprints">Editor Utility Blueprints</li>            
+            </ul>            
+        </ul>
+        Being the sole technical designer at the start of the project meant I had to wear many hats during development but
+         also held a unique insight into the project and it's systems as I had laid the groundwork for much of it. 
+         <br/>As the team grew larger I handed of more tasks to other team members and focused more on combat, player 
+         character and The Dark Man.
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/aitd/2.png" alt="Alone in the Dark Screenshot" />
+        <img class="pc-screenshot" src="img/projects/aitd/4.png" alt="Alone in the Dark Screenshot" />
+        <img class="pc-screenshot" src="img/projects/aitd/6.png" alt="Alone in the Dark Screenshot" />
+        <img class="pc-screenshot" src="img/projects/aitd/7.png" alt="Alone in the Dark Screenshot" />
+    </div>
+    `,"#1d2735",!0,!0),new e("Copoka","Copoka","img/projects/copoka/capsule_main.png",`
+    <div class="paragraph">
+        <strong>Copoka</strong> is an open world flight exploration game with a narrative beat where the player controls
+        a magpie colleting shiny objects for their nest, developed in <strong>Unreal Engine</strong>.
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/JsN-gbex6Io" frameborder="0" allowfullscreen></iframe>
+    </div>
+    
+    <div class="paragraph">
+        It started out as a student project that received an offer from Humble Bundle to be part of the back then newly founded 
+        Humble Originals in 2016. 
+        <br/>The year after we managed to pass the Steam Greenlight process and a 
+        <a href="https://store.steampowered.com/app/582620/Copoka/">Steam version</a> was released along the standalone
+        <a href="https://www.humblebundle.com/store/copoka"> Humble Store</a> version.
+    </div>
+
+    <div class="paragraph">
+        My responsibilities:
+        <ul>
+            <li>Design Lead</li>
+            <li>Prototype, pitch & presentation</li>
+            <li>Gameplay & progression design</li>
+            <li>Player character</li>
+            <ul>
+                <li>Flight</li>
+                <li>Walking</li>
+                <li>Interactions</li>
+                <li>Input</li>
+            </ul>
+            <li>Graphics optimization</li>
+            <ul>
+                <li>Lighting</li>
+                <li>Foliage</li>
+            </ul>
+            <li>Steam integration</li>
+            <li>Perforce</li>
+            <ul>
+                <li>Mentoring</li>
+                <li>Server maintenance</li>
+            </ul>
+        </ul>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/copoka/1.jpg" alt="Copoka Screenshot" />
+        <img class="pc-screenshot" src="img/projects/copoka/2.jpg" alt="Copoka Screenshot" />
+        <img class="pc-screenshot" src="img/projects/copoka/3.jpg" alt="Copoka Screenshot" />
+        <img class="pc-screenshot" src="img/projects/copoka/4.jpg" alt="Copoka Screenshot" />
+        <img class="pc-screenshot" src="img/projects/copoka/5.jpg" alt="Copoka Screenshot" />
+        <img class="pc-screenshot" src="img/projects/copoka/6.jpg" alt="Copoka Screenshot" />
+    </div>
+    `,"#5a78af"),new e("forebearers","Forebearers","img/projects/forebearers/capsule.jpg",`
+    <div class="paragraph">
+        <strong>Forebearers</strong> is a city-building strategy game puts you as the chieftain of a tribe driven away
+         from their homeland. 
+         <br/>Developed in <strong>Unreal Engine</strong> by Sandspire Interactive in early access and 
+         <a href="https://store.steampowered.com/app/923590/Forebearers/" target="_blank">released on Steam</a> 2019. 
+    </div>
+    
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/D4GdmaFBvbY" frameborder="0" allowfullscreen></iframe>
+    </div>
+    
+    <div class="paragraph">
+        I acted mainly a technical support for optimising the GPU fencing to improve performance, as well as constructed
+        the random name generator used for the villagers.
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/forebearers/1.jpg" alt="Forebearers Screenshot" />
+        <img class="pc-screenshot" src="img/projects/forebearers/2.jpg" alt="Forebearers Screenshot" />
+        <img class="pc-screenshot" src="img/projects/forebearers/3.jpg" alt="Forebearers Screenshot" />
+        <img class="pc-screenshot" src="img/projects/forebearers/4.jpg" alt="Forebearers Screenshot" />
+    </div>
+    `,"#48502d")],g=l({name:"GameProjects",components:{ProjectsList:t},data:function(){return{projects:m}}}),d=a("h1",null,"Published Games",-1),u=a("div",{style:{"margin-bottom":"30px"}}," A collection of the published games I've worked on. ",-1);function h(i,v,b,f,y,k){const s=o("ProjectsList");return p(),n("div",null,[d,u,c(s,{projects:i.projects},null,8,["projects"])])}const S=r(g,[["render",h]]);export{S as default};
